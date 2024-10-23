17a1a6f75f24afe77b882cedc7d2659d98370bb8
mahan <mahan.mehrafza@gmail.com>
2024-10-24 01:44:38 +0330

    import os

--- marriage.py
+++ marriage.py
@@ -1,11 +1,11 @@
 import time 
-
+import os 
 name_mard  = input("name mard : ")
 name_zan = input("name zan : ")
 sen_mard = input("sen mard : ")
 sen_zan = input("sen zan : ")
 
-print(clear)
+os.system('clear')
 
 print(f"gad farzand : 1 ")
 print("rang post farzand : 2")
